278196f6a846dbdae028508f55f2c77ebaa0e7dd
Engr. Rony Debnath <ronycse9@gmail.com>
2019-02-23 02:04:09 +0600

    Final Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+//file open in read mode
+$myfile = fopen("quotes.txt", "r") or die("Unable to open file!");
+$word_array = $new_word_array = array();
+
+/* The feof() function checks if the "end-of-file" (EOF) has been reached.
+   The fgets() function returns a line from an open file. 
+*/
+
+while(!feof($myfile)){
+	$word_array[] = trim(fgets($myfile)); 
+}
+ 
+//remove the empty value from the array  
+foreach($word_array as $key => $value){	 
+  if($value != ""){
+	  array_push($new_word_array,$value);		 
+  }
+}
+  
+// print the line array  from text file
+echo "<pre>";
+  print_r($new_word_array);
+echo "</pre>";
+
+//generate the random index for print random value from array
+$line = rand(0,(count($new_word_array)-1));
+
+//print the random line
+echo $new_word_array[$line];
+
+
+//close the open file
+fclose ($myfile); 
+
+?>
